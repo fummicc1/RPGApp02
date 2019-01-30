0d4fabd0f8ada851ef7921fb82b81b9d457411e4
fummicc1 <fumiya989@gmail.com>
2019-01-30 17:04:25 +0900

    first commit

--- Kobashun_RPGApp/BattleViewController.swift
+++ Kobashun_RPGApp/BattleViewController.swift
@@ -15,33 +15,33 @@ class BattleViewController: UIViewController {
     @IBOutlet var enemyImageView: UIImageView!
     @IBOutlet var enemyHPLabel: UILabel!
     
-    let enemy = Enemy()
-    let player = Player()
+    let enemy = Enemy() // 敵のインスタンス
+    let player = Player() // プレイヤーのクラス
     
-    var turnNumber: Int = 0
+    var turnNumber: Int = 0 // 現在のターン数を記録した変数
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    // 攻撃を押した時のボタン
     @IBAction func tappedAttackButton() {
         
         player.commandNumber = 0
         
         if enemy.commandNumber == 0 {
             print("攻撃！")
+            enemy.hp = enemy.hp - 1
         } else {
             print("避けられた...")
         }
         
-        enemy.hp = enemy.hp - 1
         showAlert(title: "アタック！", message: "プレイヤーは攻撃しました。")
         
         displayDamage()
-        
-        doEnemyTurn()
     }
     
+    // 避けるを押した時のボタン
     @IBAction func tappedAvoidButton() {
         
         player.commandNumber = 1
@@ -49,11 +49,9 @@ class BattleViewController: UIViewController {
         showAlert(title: "避けましょう！", message: "プレイヤーは次のターン攻撃を喰らいません。")
         
         displayDamage()
-        
-        doEnemyTurn()
     }
     
-    // ここがわ分からなかったら#7のメモを復習しよう！
+    // ここは#7のメモの教科書を使用しているよ！
     func showAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
@@ -64,7 +62,7 @@ class BattleViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    // 偶数ならば、自分のターン。奇数ならば相手のターン。
+    // 偶数ならば、自分のターン。奇数ならば相手のターン。 ← 重要
     func judgeTurn() {
         if turnNumber % 2 == 0 {
             print("プレイヤーのターン")
@@ -74,6 +72,7 @@ class BattleViewController: UIViewController {
         }
     }
     
+    // 敵にダメージを与えるかメソッド(教科書#5と#6)
     func doEnemyTurn() {
         enemy.commandNumber = Int.random(in: 0...1) // 攻撃するか避けるかの選択を決める。
         if enemy.commandNumber % 2 == 0 {
@@ -87,11 +86,10 @@ class BattleViewController: UIViewController {
         } else {
             showAlert(title: "...", message: "敵がコソコソしているぞ..?")
         }
-        
         displayDamage()
-        
     }
     
+    // プレイヤーと敵のHPをラベルに表示する。
     func displayDamage() {
         playerHPLabel.text = String(player.hp)
         enemyHPLabel.text = String(enemy.hp)
